139dc7e41d31fdcde4604ad8bc582c046d88d28f
Olle-svg <ollelegnedahl@gmail.com>
2026-02-25 01:53:40 +0100

    Add files via upload

---
+++ Sätta betyg.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Betyg : MonoBehaviour
+{
+    public GameObject betyg;
+
+    void Start()
+    {
+
+    }
+
+    public void sattbetyg()
+    {
+        betyg.SetActive(true);
+    }
+}
